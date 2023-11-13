90ac0618da84b5dca37274f46e7a615092044626
220296 <220296@student.glu.nl>
2023-11-13 10:33:20 +0100

    pause menu
    full pause menu 👌

---
+++ Victorian Racer/Assets/Resource/Scripts/pauseMenu.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class pauseMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject pausemenu;
+    [SerializeField] private bool IsPaused = false;
+    [SerializeField] private KeyCode pauseKey = KeyCode.Escape;
+    private void Start()
+    {
+        pausemenu.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(pauseKey))
+        {
+            if (IsPaused)
+            {
+                resumeGame();
+            }
+            else
+            {
+                pauseGame();
+            }
+        }
+    }
+
+    public void pauseGame()
+    {
+        pausemenu.SetActive(true);
+        Time.timeScale = 0.0f;
+        IsPaused = true;
+    }
+
+    public void resumeGame()
+    {
+        pausemenu.SetActive(false);
+        Time.timeScale = 1.0f;
+        IsPaused = false;
+    }
+
+    public void backToMain()
+    {
+        SceneManager.LoadScene(0);
+        IsPaused = false;
+        Time.timeScale = 1.0f;
+    }
+}
