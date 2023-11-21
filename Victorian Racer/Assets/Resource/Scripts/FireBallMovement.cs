612030edb57419bbb7f2ea085cdeedf47564c1a4
Obayouh <80900668+Obayouh@users.noreply.github.com>
2023-11-21 23:42:53 +0100

    My Push for Sprint 2
    -Using the pitchfork models for the trap now
    -New Fireball effect added to fireball prefab
    -Fireball Move script tweaked slightly
    -Main Race had trap box and fireball trigger added

--- Victorian Racer/Assets/Resource/Scripts/FireBallMovement.cs
+++ Victorian Racer/Assets/Resource/Scripts/FireBallMovement.cs
@@ -8,14 +8,16 @@ public class FireBallMovement : MonoBehaviour
 
     private float destroyTimer = 5f;
 
+    private void Start()
+    {
+        Invoke("DestroyFireball", destroyTimer);
+    }
+
     void Update()
     {
+
         transform.Translate(_moveSpeed * Time.deltaTime * Vector3.forward);
 
-        if (destroyTimer <= 0)
-        {
-            Invoke("DestroyFireball", destroyTimer);
-        }
     }
 
     private void OnTriggerEnter(Collider other)
