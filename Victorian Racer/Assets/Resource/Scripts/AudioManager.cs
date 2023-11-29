a34238714c5d3591accaef6a814da2097ccb449e
Obayouh <80900668+Obayouh@users.noreply.github.com>
2023-11-29 12:35:50 +0100

    Last updates for now

--- Victorian Racer/Assets/Resource/Scripts/AudioManager.cs
+++ Victorian Racer/Assets/Resource/Scripts/AudioManager.cs
@@ -36,15 +36,13 @@ public class AudioManager : MonoBehaviour
         _pitchforkTrap = FindObjectOfType<PitchforkTrap>();
         _fireBallAbility = FindObjectOfType<FireBallAbility>();
 
-        _chantVolume = _mainMenuChant.volume;
-        _driftVolume = _carControls._carDriveSFX.volume;
-        _driftVolume = _carControls._driftSFX.volume;
-        _BGMVolume = _carControls._BGM.volume;
-        _bellVolume = _respawnCheckpoints._bellSFX.volume;
-        _graveyardLeftVolume = _respawnCheckpoints._graveyardBGMLeft.volume;
-        _graveyardRightVolume = _respawnCheckpoints._graveyardBGMRight.volume;
-        _clangVolume = _pitchforkTrap._clangSFX.volume;
-        _wooshVolume = _fireBallAbility._fireWooshSFX.volume;
+
+    }
+
+    private void Start()
+    {
+        //MuteSound();
+
     }
 
     private void Update()
@@ -53,51 +51,83 @@ public class AudioManager : MonoBehaviour
         //{
         //    _mute = true;
         //}
-        
+
         //if (_uncheckedAudioBox.activeInHierarchy == true)
         //{
         //    _mute = false;
         //}
+    }
+
+    public void MuteSound()
+    {
+        _mute = true;
 
         if (_mute == true)
         {
             if (SceneManager.sceneCount == 0)
             {
-                _mainMenuChant.volume = 0.0f;
+                //_mainMenuChant.volume = 0.0f;
+                _mainMenuChant.mute = true;
             }
-            
             if (SceneManager.sceneCount == 1)
             {
-                _carControls._carDriveSFX.volume = 0.0f;
-                _carControls._driftSFX.volume = 0.0f;
-                _carControls._BGM.volume = 0.0f;
-                _respawnCheckpoints._bellSFX.volume = 0.0f;
-                _respawnCheckpoints._graveyardBGMLeft.volume = 0.0f;
-                _respawnCheckpoints._graveyardBGMRight.volume = 0.0f;
-                _raceCountdown._carStartSFX.volume = 0.0f;
-                _pitchforkTrap._clangSFX.volume = 0.0f;
-                _fireBallAbility._fireWooshSFX.volume = 0.0f;
+                //_carControls._carDriveSFX.volume = 0.0f;
+                //_carControls._driftSFX.volume = 0.0f;
+                //_carControls._BGM.volume = 0.0f;
+                //_respawnCheckpoints._bellSFX.volume = 0.0f;
+                //_respawnCheckpoints._graveyardBGMLeft.volume = 0.0f;
+                //_respawnCheckpoints._graveyardBGMRight.volume = 0.0f;
+                //_raceCountdown._carStartSFX.volume = 0.0f;
+                //_pitchforkTrap._clangSFX.volume = 0.0f;
+                //_fireBallAbility._fireWooshSFX.volume = 0.0f;
+                _carControls._carDriveSFX.mute = true;
+                _carControls._driftSFX.mute = true;
+                _carControls._BGM.mute = true;
+                _respawnCheckpoints._bellSFX.mute = true;
+                _respawnCheckpoints._graveyardBGMLeft.mute = true;
+                _respawnCheckpoints._graveyardBGMRight.mute = true;
+                _raceCountdown._carStartSFX.mute = true;
+                _pitchforkTrap._clangSFX.mute = true;
+                _fireBallAbility._fireWooshSFX.mute = true;
+            }
+            else
+            {
+                ;
             }
         }
+    }
 
-        if (_mute == true)
+    public void UnMuteSound()
+    {
+        _mute = false;
+
+        if (_mute == false)
         {
             if (SceneManager.sceneCount == 0)
             {
-                _mainMenuChant.volume = _chantVolume;
+                _mainMenuChant.mute = false;
             }
-            
+
             if (SceneManager.sceneCount == 1)
             {
-                _carControls._carDriveSFX.volume = _driveVolume;
-                _carControls._driftSFX.volume = _driftVolume;
-                _carControls._BGM.volume = _BGMVolume;
-                _respawnCheckpoints._bellSFX.volume = _bellVolume;
-                _respawnCheckpoints._graveyardBGMLeft.volume = _graveyardLeftVolume;
-                _respawnCheckpoints._graveyardBGMRight.volume = _graveyardRightVolume;
-                _raceCountdown._carStartSFX.volume = _startVolume;
-                _pitchforkTrap._clangSFX.volume = _clangVolume;
-                _fireBallAbility._fireWooshSFX.volume = _wooshVolume;
+                //_carControls._carDriveSFX.volume = _driveVolume;
+                //_carControls._driftSFX.volume = _driftVolume;
+                //_carControls._BGM.volume = _BGMVolume;
+                //_respawnCheckpoints._bellSFX.volume = _bellVolume;
+                //_respawnCheckpoints._graveyardBGMLeft.volume = _graveyardLeftVolume;
+                //_respawnCheckpoints._graveyardBGMRight.volume = _graveyardRightVolume;
+                //_raceCountdown._carStartSFX.volume = _startVolume;
+                //_pitchforkTrap._clangSFX.volume = _clangVolume;
+                //_fireBallAbility._fireWooshSFX.volume = _wooshVolume;
+                _carControls._carDriveSFX.mute = false;
+                _carControls._driftSFX.mute = false;
+                _carControls._BGM.mute = false;
+                _respawnCheckpoints._bellSFX.mute = false;
+                _respawnCheckpoints._graveyardBGMLeft.mute = false;
+                _respawnCheckpoints._graveyardBGMRight.mute = false;
+                _raceCountdown._carStartSFX.mute = false;
+                _pitchforkTrap._clangSFX.mute = false;
+                _fireBallAbility._fireWooshSFX.mute = false;
             }
         }
     }
